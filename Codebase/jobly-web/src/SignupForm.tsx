4f9a87ca61f0662c929143fc555303cb5e91ddad
Gaurav Soni <grvsoniuk@gmail.com>
2023-07-03 00:20:25 +0100

    Change UI from Ant Design to Bootstrap

--- Codebase/jobly-web/src/SignupForm.tsx
+++ Codebase/jobly-web/src/SignupForm.tsx
@@ -1,132 +1,178 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, notification } from 'antd';
+import axios from 'axios';
 
-const SignupForm = () => {
-  const [loading, setLoading] = useState(false);
+const SignupForm: React.FC = () => {
+  const [formData, setFormData] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
 
-  const onFinish = async (values: any) => {
-    setLoading(true);
+  const [formErrors, setFormErrors] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
 
-    // Prepare the payload
-    const payload = {
-      firstname: values.firstname,
-      lastname: values.lastname,
-      email: values.email,
-      password: values.password,
-    };
-
-    try {
-      // Call the backend API
-      const response = await fetch('http://localhost:8080/user/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-      });
-
-      // Handle the response
-      if (response.ok) {
-        // Signup successful
-        setTimeout(() => {
-            setLoading(false);
-            notification.success({
-              message: 'Signup Successful',
-              description: 'You have been successfully signed up!',
-            });
-          }, 1000);
-      } else {
-        // Signup failed
-        setTimeout(() => {
-            setLoading(false);
-            notification.error({
-              message: 'Signup Failed',
-              description: 'Something went wrong, please retry!',
-            });
-          }, 1000);
-      }
-    } catch (error) {
-        setTimeout(() => {
-            setLoading(false);
-            notification.error({
-              message: 'Signup Failed',
-              description: 'Something went wrong, please retry!',
-            });
-          }, 1000);
-    }
-
-    setLoading(false);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+  const validateForm = () => {
+    let isValid = true;
+    const newFormErrors:  { firstname: string; lastname: string; email: string; password: string; confirm_password: string; } = {firstname: "", lastname: "", email: "", password: "", confirm_password: ""};
+
+    // Validate firstname
+    if (!formData.firstname) {
+      isValid = false;
+      newFormErrors.firstname = 'First Name is required';
+    }
+
+    // Validate lastname
+    if (!formData.lastname) {
+      isValid = false;
+      newFormErrors.lastname = 'Last Name is required';
+    }
+
+    // Validate email
+    if (!formData.email) {
+      isValid = false;
+      newFormErrors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      isValid = false;
+      newFormErrors.email = 'Invalid email format';
+    }
+
+    // Validate password
+    if (!formData.password) {
+      isValid = false;
+      newFormErrors.password = 'Password is required';
+    }
+
+    // Validate confirm_password
+    if (!formData.confirm_password) {
+      isValid = false;
+      newFormErrors.confirm_password = 'Confirm Password is required';
+    } else if (formData.password !== formData.confirm_password) {
+      isValid = false;
+      newFormErrors.confirm_password = 'Passwords do not match';
+    }
+
+    setFormErrors(newFormErrors);
+    return isValid;
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (validateForm()) {
+      // Perform API call using Axios or Fetch here
+      try {
+        await axios.post('http://localhost:8080/user/signup', {
+          firstname: formData.firstname,
+          lastname: formData.lastname,
+          email: formData.email,
+          password: formData.password,
+        });
+        // Handle successful form submission
+        alert('Signup successful!');
+      } catch (error) {
+        // Handle error in form submission
+        alert('Signup failed. Please try again later.');
+      }
+    }
   };
 
   return (
-    <div style={{ width: 400, margin: '50px auto 0', border: '1px solid #ddd', borderRadius: 15, boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)', background: 'linear-gradient(to bottom right, #f7f7f7, #e9e9e9)' }}>
-      <h3 style={{ textAlign: 'center', padding: '20px 0', background: '#1890ff', color: '#fff', margin: 0, borderTopLeftRadius: 15, borderTopRightRadius: 15 }}>Sign up</h3>
-      <Form
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        layout="vertical"
-        style={{ padding: '20px' }}
-      >
-        <Form.Item
-          label="First Name"
-          name="firstname"
-          rules={[{ required: true, message: 'Please enter your first name' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Last Name"
-          name="lastname"
-          rules={[{ required: true, message: 'Please enter your last name' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: 'Please enter your email' }]}
-        >
-          <Input type="email" />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please enter your password' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          label="Confirm Password"
-          name="confirm_password"
-          rules={[
-            { required: true, message: 'Please confirm your password' },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error('The two passwords do not match'));
-              },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit" loading={loading} block>
-            Sign up
-          </Button>
-        </Form.Item>
-      </Form>
+    <div
+      style={{
+        width: '300px',
+        margin: '100px auto 0',
+        padding: '20px',
+        borderRadius: '15px',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+        background: 'linear-gradient(to right, #c4d3f6, #f3e7ff)',
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-center mb-4">Signup</h3>
+        <div className="mb-3">
+          <input
+            type="text"
+            name="firstname"
+            className="form-control"
+            placeholder="First Name"
+            value={formData.firstname}
+            onChange={handleChange}
+          />
+          {formErrors.firstname && (
+            <div className="text-danger">{formErrors.firstname}</div>
+          )}
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            name="lastname"
+            className="form-control"
+            placeholder="Last Name"
+            value={formData.lastname}
+            onChange={handleChange}
+          />
+          {formErrors.lastname && (
+            <div className="text-danger">{formErrors.lastname}</div>
+          )}
+        </div>
+        <div className="mb-3">
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {formErrors.email && (
+            <div className="text-danger">{formErrors.email}</div>
+          )}
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {formErrors.password && (
+            <div className="text-danger">{formErrors.password}</div>
+          )}
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            name="confirm_password"
+            className="form-control"
+            placeholder="Confirm Password"
+            value={formData.confirm_password}
+            onChange={handleChange}
+          />
+          {formErrors.confirm_password && (
+            <div className="text-danger">{formErrors.confirm_password}</div>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary w-100">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
