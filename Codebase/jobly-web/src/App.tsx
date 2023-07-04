95773a176268de9814ca797a364591b331e70dfb
Gaurav Soni <grvsoniuk@gmail.com>
2023-07-04 19:49:32 +0100

    Removed package with high vurnabilities

--- Codebase/jobly-web/src/App.tsx
+++ Codebase/jobly-web/src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container">
           <a className="navbar-brand" href="/">
-            <img src='logo-m.png'></img> Jobly
+            <img src='logo-m.png' alt='Logo'></img> <span>Jobly</span>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
