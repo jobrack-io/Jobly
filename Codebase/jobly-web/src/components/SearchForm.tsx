f6840a69aac8af30da0b2c681b56c423a98955ea
Gaurav Soni <grvsoniuk@gmail.com>
2023-07-10 19:09:12 +0100

    convert serach form to typescript code

--- Codebase/jobly-web/src/components/SearchForm.tsx
+++ Codebase/jobly-web/src/components/SearchForm.tsx
@@ -1,42 +1,81 @@
 import axios from "axios";
 import { useState } from "react";
 
+const apiUrl = 'http://localhost:8000/search/jobs';
+
+type SkillLevel = 'Beginner' | 'Intermediate' | 'Advanced';
+type CompanySize = 'Small' | 'Medium' | 'Large';
+type Location = 'New York' | 'London' | 'Tokyo';
+type Skill = 'React' | 'JavaScript' | 'Node.js' | 'Python' | 'Java' | 'Ruby';
+type JobType = 'Full-time' | 'Part-time' | 'Contract';
+
+interface JobSearchFilter {
+  text: string;
+  company: string;
+  skillLevels: SkillLevel[];
+  companySize: CompanySize | '';
+  locations: Location[];
+  skills: Skill[];
+  salaryMin: number;
+  salaryMax: number;
+  jobTypes: JobType[];
+}
+
 function SearchForm() {
 
-    const [text, setText] = useState('');
-  const [company, setCompany] = useState('');
-  const [skillLevels, setSkillLevels] = useState([]);
-  const [companySize, setCompanySize] = useState('');
-  const [locations, setLocations] = useState([]);
-  const [skills, setSkills] = useState([]);
-  const [salaryMin, setSalaryMin] = useState(0);
-  const [salaryMax, setSalaryMax] = useState(100000);
-  const [jobTypes, setJobTypes] = useState([]);
+  const [filters, setFilters] = useState<JobSearchFilter>({
+    text: '',
+    company: '',
+    skillLevels: [],
+    companySize: '',
+    locations: [],
+    skills: [],
+    salaryMin: 0,
+    salaryMax: 100000,
+    jobTypes: [],
+  });
 
-  const handleSearch = async (e) => {
-    e.preventDefault();
-
-    const payload = {
-      text,
-      company,
-      skillLevels,
-      companySize,
-      locations,
-      skills,
-      salary: { min: salaryMin, max: salaryMax },
-      jobTypes
-    };
-
-    try {
-      const response = await axios.post('http://localhost:8000/search/jobs', payload);
-      console.log(response.data); // Do something with the response data
-    } catch (error) {
-      console.error(error);
-    }
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
   };
 
+  const handleMultiSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, options } = event.target;
+    const selectedOptions = Array.from(options)
+      .filter((option) => option.selected)
+      .map((option) => option.value);
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: selectedOptions,
+    }));
+  };
+
+  const handleSalaryRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: parseInt(value),
+    }));
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios.post(apiUrl, filters)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+
   return (
-      <form onSubmit={handleSearch} style={
+      <form onSubmit={handleFormSubmit} style={
         {
             width: '90%',
             margin: '25px 0px',
@@ -47,58 +86,64 @@ function SearchForm() {
           }
     }>
         <div className="mb-3">
-          <label htmlFor="text" className="form-label">Text</label>
-          <input type="text" id="text" className="form-control" value={text} onChange={(e) => setText(e.target.value)} />
+          <label htmlFor="text">Text</label>
+          <input type="text" className="form-control" id="text" name="text" value={filters.text} onChange={handleFilterChange} />
         </div>
         <div className="mb-3">
-          <label htmlFor="company" className="form-label">Company</label>
-          <input type="text" id="company" className="form-control" value={company} onChange={(e) => setCompany(e.target.value)} />
+          <label htmlFor="company">Company</label>
+          <input type="text" className="form-control" id="company" name="company" value={filters.company} onChange={handleFilterChange} />
         </div>
         <div className="mb-3">
-          <label htmlFor="skillLevels" className="form-label">Skill Levels</label>
-          <select multiple id="skillLevels" className="form-select" value={skillLevels} onChange={(e) => setSkillLevels(Array.from(e.target.selectedOptions, option => option.value))}>
-            <option value="beginner">Beginner</option>
-            <option value="intermediate">Intermediate</option>
-            <option value="advanced">Advanced</option>
+          <label htmlFor="skillLevels">Skill Levels</label>
+          <select multiple className="form-select" id="skillLevels" name="skillLevels" value={filters.skillLevels} onChange={handleMultiSelectChange}>
+            <option value="Beginner">Beginner</option>
+            <option value="Intermediate">Intermediate</option>
+            <option value="Advanced">Advanced</option>
           </select>
         </div>
         <div className="mb-3">
-          <label htmlFor="companySize" className="form-label">Company Size</label>
-          <input type="text" id="companySize" className="form-control" value={companySize} onChange={(e) => setCompanySize(e.target.value)} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="locations" className="form-label">Locations</label>
-          <select multiple id="locations" className="form-select" value={locations} onChange={(e) => setLocations(Array.from(e.target.selectedOptions, option => option.value))}>
-            <option value="location1">Location 1</option>
-            <option value="location2">Location 2</option>
-            <option value="location3">Location 3</option>
+          <label htmlFor="companySize">Company Size</label>
+          <select className="form-select" id="companySize" name="companySize" value={filters.companySize} onChange={handleFilterChange}>
+            <option value="">Any</option>
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
           </select>
         </div>
         <div className="mb-3">
-          <label htmlFor="skills" className="form-label">Skills</label>
-          <select multiple id="skills" className="form-select" value={skills} onChange={(e) => setSkills(Array.from(e.target.selectedOptions, option => option.value))}>
-            <option value="skill1">Skill 1</option>
-            <option value="skill2">Skill 2</option>
-            <option value="skill3">Skill 3</option>
+          <label htmlFor="locations">Locations</label>
+          <select multiple className="form-select" id="locations" name="locations" value={filters.locations} onChange={handleMultiSelectChange}>
+            <option value="New York">New York</option>
+            <option value="London">London</option>
+            <option value="Tokyo">Tokyo</option>
           </select>
         </div>
         <div className="mb-3">
-          <label htmlFor="salary" className="form-label">Salary</label>
-          <input type="range" id="salary" className="form-range" min="0" max="100000" value={salaryMin} onChange={(e) => setSalaryMin(parseInt(e.target.value))} />
-          <input type="range" id="salary" className="form-range" min="0" max="100000" value={salaryMax} onChange={(e) => setSalaryMax(parseInt(e.target.value))} />
+          <label htmlFor="skills">Skills</label>
+          <select multiple className="form-select" id="skills" name="skills" value={filters.skills} onChange={handleMultiSelectChange}>
+            <option value="React">React</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="Node.js">Node.js</option>
+            <option value="Python">Python</option>
+            <option value="Java">Java</option>
+            <option value="Ruby">Ruby</option>
+          </select>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="salaryRange">Salary Range</label>
+          <input type="range" className="form-range" id="salaryMin" name="salaryMin" min="0" max="100000" step="1000" value={filters.salaryMin} onChange={handleSalaryRangeChange} />
+          <input type="range" className="form-range" id="salaryMax" name="salaryMax" min="0" max="100000" step="1000" value={filters.salaryMax} onChange={handleSalaryRangeChange} />
           <div>
-            Min: ${salaryMin}
-          </div>
-          <div>
-            Max: ${salaryMax}
+            Min: ${filters.salaryMin.toLocaleString()}
+            Max: ${filters.salaryMax.toLocaleString()}
           </div>
         </div>
         <div className="mb-3">
-          <label htmlFor="jobTypes" className="form-label">Job Types</label>
-          <select multiple id="jobTypes" className="form-select" value={jobTypes} onChange={(e) => setJobTypes(Array.from(e.target.selectedOptions, option => option.value))}>
-            <option value="fullTime">Full Time</option>
-            <option value="partTime">Part Time</option>
-            <option value="contract">Contract</option>
+          <label htmlFor="jobTypes">Job Types</label>
+          <select multiple className="form-select" id="jobTypes" name="jobTypes" value={filters.jobTypes} onChange={handleMultiSelectChange}>
+            <option value="Full-time">Full-time</option>
+            <option value="Part-time">Part-time</option>
+            <option value="Contract">Contract</option>
           </select>
         </div>
         <button type="submit" className="btn btn-primary">Search</button>
