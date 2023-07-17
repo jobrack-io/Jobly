83a9c38a25e279dee2dec6009a6dddc3b1406856
rajnautiyal <nautiyal.rajendra@gmail.com>
2023-07-17 11:40:29 +0100

    only fetching first user

--- Codebase/jobly-authentication/src/main/java/com/jobly/api/repository/UserRepository.java
+++ Codebase/jobly-authentication/src/main/java/com/jobly/api/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.jobly.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface UserRepository extends JpaRepository<UserEntity, String> {
-    UserEntity findUserByEmail(String email);
+    UserEntity findFirstUserByEmail(String email);
 
 
 }
