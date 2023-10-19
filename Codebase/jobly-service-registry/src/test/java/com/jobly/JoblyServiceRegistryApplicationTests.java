2b55010f01afa4019aadea56e0be1f38fc43e64d
rajnautiyal <nautiyal.rajendra@gmail.com>
2023-10-19 21:33:41 +0100

    added jobly

---
+++ Codebase/jobly-service-registry/src/test/java/com/jobly/JoblyServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblyServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
